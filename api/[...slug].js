5f4660ee6dad4ebb6c313ee1a91e424f6c71ba11
Sreecharan01 <150925486+Sreecharan01@users.noreply.github.com>
2026-07-07 14:48:17 +0530

    chore: remove root Vercel serverless setup and add client SPA config

--- api/[...slug].js
+++
@@ -1,8 +0,0 @@
-const app = require('../server/server.js');
-
-module.exports = (req, res) => {
-  if (!req.url.startsWith('/api')) {
-    req.url = `/api${req.url === '/' ? '' : req.url}`;
-  }
-  return app(req, res);
-};
